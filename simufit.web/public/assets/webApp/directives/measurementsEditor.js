d292cd68752cb5022631fae7e3f1bb084c26063f
mbalex99 <maxalex@zinkpulse.com>
2013-07-11 10:38:29 -0400

    removing all js to  a central location

---
+++ simufit.web/public/assets/webApp/directives/measurementsEditor.js
@@ -0,0 +1,16 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Maximilian
+ * Date: 7/11/13
+ * Time: 1:37 AM
+ * To change this template use File | Settings | File Templates.
+ */
+Application.Directives.directive('measurementsEditor', function(){
+   return{
+       restrict: 'EAC',
+       templateUrl: 'partials/measurements-editor.html'
+       scope:{
+           ngModel: '='
+       }
+   };
+});
