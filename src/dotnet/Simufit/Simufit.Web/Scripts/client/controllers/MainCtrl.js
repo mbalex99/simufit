93b52d306c8252312f1b2187c2327ce798d2b40f
mbalex99 <maxalex@zinkpulse.com>
2013-10-24 01:51:07 -0400

    adding task

--- src/dotnet/Simufit/Simufit.Web/Scripts/client/controllers/MainCtrl.js
+++ src/dotnet/Simufit/Simufit.Web/Scripts/client/controllers/MainCtrl.js
@@ -2,10 +2,12 @@
 
 Application.Controllers.controller('MainCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
 
-    $scope.gigs = [
-        {
-            title: '20 pushups',
-            isDone: true,
-        }
-    ];
+    $scope.entry = {
+        gigs: [{ title: '20 pushups', isDone: false }, { title: "Sprint 45 meters", isDone: true }],
+        measurements: [
+            { title: 'Weight', value: 175, unit: 'lbs' },
+            { title: 'Height', value: 72, unit: 'in' },
+            { title: 'Height', value: 72, unit: 'in' }
+        ]
+    };
 }]);
--- src/dotnet/Simufit/Simufit.Web/Scripts/client/directives/entry.js
+++ src/dotnet/Simufit/Simufit.Web/Scripts/client/directives/entry.js
@@ -1,27 +1,54 @@
 ﻿'use strict';
 
-Application.Directives.directive('entry', function() {
+Application.Directives.directive('entry', function () {
     return {
         restrict: 'EA',
         templateUrl: 'partials/entry.html',
         scope: {
-            gigs: "=gigs",
-            measurements: '=measurements'
+            entry: "="
         },
-        link: function(scope, element, attrs) {
-            
+        link: function (scope, element, attrs) {
+
+            scope.activeTab = scope.activeTab || 'list';
+
+            scope.switchTab = function (tabName) {
+                scope.activeTab = tabName;
+            };
+
         },
-        controller: 'EntryCtrl'
+        controller: ['$scope', function ($scope) {
+
+            var isNotEmptyNullOrWhiteSpace = function (myString) {
+                if (/\S/.test(myString)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            };
+
+            $scope.createNewTask = function () {
+                $scope.$apply(function() {
+                    $scope.entry.gigs.push({
+                        title: "",
+                        isDone: false
+                    });
+                });
+                
+            };
+
+            $scope.removeTask = function(index) {
+                $scope.$apply(function() {
+                    $scope.entry.gigs.splice(index, 1);
+                });
+            };
+
+            $scope.enterKeyPressed = function (index) {
+                var gig = $scope.entry.gigs[index];
+                
+                if (isNotEmptyNullOrWhiteSpace(gig.title)) {
+                    $scope.createNewTask();
+                }
+            };
+        }]
     };
 });
-
-
-Application.Controllers.controller('EntryCtrl', ['$scope', function($scope) {
-
-    $scope.activeTab = $scope.activeTab || 'list';
-
-    $scope.switchTab = function(tabName) {
-        $scope.activeTab = tabName;
-    };
-
-}]);
